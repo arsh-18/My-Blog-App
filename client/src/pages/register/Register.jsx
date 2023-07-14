50024e1649f8a65208b07aef3d900b7c7ba26369
Arsh Poddar <arshpoddar4@gmail.com>
2023-07-14 23:41:50 +0530

    commit

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
+      const res = await axios.post("https://blog-app-5l9z.onrender.com/api/auth/register", {
         username,
         email,
         password,
